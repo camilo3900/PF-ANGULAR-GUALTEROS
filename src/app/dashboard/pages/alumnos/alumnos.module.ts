79dc3b91f750c78edae56793260f17907cfb57ff
camilo3900 <camilogv.1629@gmail.com>
2023-10-12 12:05:08 -0500

    Creacion de carpetas

---
+++ src/app/dashboard/pages/alumnos/alumnos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlumnosComponent } from './alumnos.component';
+
+
+
+@NgModule({
+  declarations: [
+    AlumnosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AlumnosModule { }
